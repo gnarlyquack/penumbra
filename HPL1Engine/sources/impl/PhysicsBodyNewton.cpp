4f79e85d2e56a31a79aac0a6870c4f58cd89cd16
Karl Nack <karl.nack@fastmail.com>
2025-01-25 03:49:52 -0600

    Fix taking the address of an rvalue

--- HPL1Engine/sources/impl/PhysicsBodyNewton.cpp
+++ HPL1Engine/sources/impl/PhysicsBodyNewton.cpp
@@ -101,7 +101,8 @@ namespace hpl {
 		if(cPhysicsBodyNewton::mbUseCallback==false) return;
 
 		cPhysicsBodyNewton *pRigidBody = static_cast<cPhysicsBodyNewton*>(apEntity);
-		NewtonBodySetMatrix(pRigidBody->mpNewtonBody, &apEntity->GetLocalMatrix().GetTranspose().m[0][0]);
+		cMatrixf matrix = apEntity->GetLocalMatrix().GetTranspose();
+		NewtonBodySetMatrix(pRigidBody->mpNewtonBody, &matrix.m[0][0]);
 
 		if(pRigidBody->mpNode) pRigidBody->mpNode->SetMatrix(apEntity->GetLocalMatrix());
 	}

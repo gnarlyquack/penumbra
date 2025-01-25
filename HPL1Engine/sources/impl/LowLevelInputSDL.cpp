d5b2664f402bf61cb06dea3656bca04143775f15
Karl Nack <karl.nack@fastmail.com>
2025-01-25 15:53:26 -0600

    Stop grabbing input, at least for now
    This makes it impossible to break into a debugger.

--- HPL1Engine/sources/impl/LowLevelInputSDL.cpp
+++ HPL1Engine/sources/impl/LowLevelInputSDL.cpp
@@ -53,7 +53,9 @@ namespace hpl {
 
 	void cLowLevelInputSDL::LockInput(bool abX)
 	{
+#if 0
 		SDL_WM_GrabInput(abX ? SDL_GRAB_ON : SDL_GRAB_OFF);
+#endif
 	}
 
 	//-----------------------------------------------------------------------
